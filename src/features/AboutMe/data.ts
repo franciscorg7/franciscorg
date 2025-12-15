7c2ca69fa4839e2f294b69b8bea618021280b1be
Francisco Gonçalves <francisco.rodrigues.goncalves@cgd.pt>
2025-12-15 18:02:05 +0000

    🎨 working w/ blobs

--- src/features/AboutMe/data.ts
+++ src/features/AboutMe/data.ts
@@ -1,20 +1,28 @@
 import { AboutMeTranslationKey } from '../../config/translation-keys'
 import type { CompetenceItem } from './types'
 
+const CompetenceItemId = {
+  FRONTEND: 'frontend',
+  TEAM_MENTORING: 'team-mentoring',
+  DELIVERY_OWNERSHIP: 'delivery-ownership',
+} as const
+
 export const competences: CompetenceItem[] = [
   {
-    id: 'frontend',
+    id: CompetenceItemId.FRONTEND,
     translationKey: AboutMeTranslationKey.COMPETENCES_FRONTEND,
     className: 'bg-blue-200 text-blue-800',
   },
   {
-    id: 'team-mentoring',
+    id: CompetenceItemId.TEAM_MENTORING,
     translationKey: AboutMeTranslationKey.COMPETENCES_TEAM_MENTORING,
     className: 'bg-green-200 text-green-800',
   },
   {
-    id: 'delivery-ownership',
+    id: CompetenceItemId.DELIVERY_OWNERSHIP,
     translationKey: AboutMeTranslationKey.COMPETENCES_DELIVERY_OWNERSHIP,
     className: 'bg-yellow-200 text-yellow-800',
   },
 ]
+
+export const NAME_LABEL = 'Francisco Gonçalves'
